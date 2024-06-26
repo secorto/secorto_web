c06e42078c984af9ce2966c0748546f7d817af07
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-06-26 01:33:20 -0500

    Actualizada configuración añadidos redirects a charlas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro.secorto.com"
+  site: "https://secorto.com",
+  redirects: {
+    '/blog/2023-09-27-devcontainers': '/charla/2023-09-27-devcontainers',
+    '/blog/2022-08-14-screenpy': '/charla/2022-08-14-screenpy',
+    '/blog/2020-08-03-que-es-markdown': '/charla/2020-08-03-que-es-markdown',
+    '/blog/2019-10-22-blog-con-gatsby': '/charla/2019-10-22-blog-con-gatsby',
+    '/blog/2018-09-17-patrones-automatizacion-pruebas': '/charla/2018-09-17-patrones-automatizacion-pruebas',
+    '/blog/2017-01-30-test-unitarios': '/charla/2017-01-30-test-unitarios'
+  }
 });
