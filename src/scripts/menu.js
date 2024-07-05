e5c5ffe3d6024161f58577332bb1cb6ca73f579a
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-07-05 03:40:21 +0000

    Sidebar copiado de secorto.com

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,13 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+  document.querySelector('.sidebar-toggle').classList.toggle('sidebar-open');
+  document.querySelector('.hamburger').classList.toggle('sidebar-open');
+  localStorage.setItem("sidebarOpen", JSON.stringify(document.querySelector('.sidebar-toggle').classList.contains('sidebar-open')))
 });
+
+if(window.matchMedia("(min-width: 800px)").matches === true) {
+  const isOpen = JSON.parse(localStorage.getItem("sidebarOpen"))
+  if(isOpen) {
+    document.querySelector('.hamburger').classList.toggle('sidebar-open');
+    document.querySelector('.sidebar-toggle').classList.toggle('sidebar-open');
+  }
+}
