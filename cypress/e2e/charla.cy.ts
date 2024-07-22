eea9eab395f3d04d1b048d4bbb9b44f4c6caa09f
Sergio Orozco <scot3004@gmail.com>
2024-07-22 02:12:31 -0500

    Charla test

---
+++ cypress/e2e/charla.cy.ts
@@ -0,0 +1,7 @@
+it('Blog post se visualiza', () => {
+  const page = cy.visit('/charla');
+  page.get('head title').should('have.text', 'Charlas | SeCOrTo')
+  page.get('header h1').should('have.text', 'Charlas');
+  page.get('[href="/charla/2023-09-27-devcontainers"]').click()
+  page.get('header h1').should('have.text', 'Devcontainers en localhost');
+});
