fdd34c516d8f76643c13f06e8779ed61b3222556
Sergio Orozco <scot3004@gmail.com>
2024-07-22 22:11:19 -0500

    Overflow-wrap fix

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -4,4 +4,7 @@ it('Blog post se visualiza', () => {
   page.get('header h1').should('have.text', 'Blog');
   page.get('[href="/blog/2022-08-14-poo-python"]').click()
   page.get('header h1').should('have.text', 'Programación orientada a objetos en python');
+  page.viewport('iphone-6')
+  cy.scrollTo('right');
+  cy.window().its('scrollX').should('eq', 0)
 });
