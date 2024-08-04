0d22d37d57d548e44f281e183d75ea27726bb40b
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-08-03 22:59:10 -0500

    Upgrade ago (#21)
    * Organize tests and checks
    
    * Upgrade packages

--- cypress/e2e/main-page.cy.ts
+++ cypress/e2e/main-page.cy.ts
@@ -1,6 +1,7 @@
-it('los titulos son correctos', () => {
+it('Pagina de inicio', () => {
   const page = cy.visit('/');
-
   page.get('title').should('have.text', 'Página de inicio | SeCOrTo')
   page.get('h1').should('have.text', 'Soy Sergio Carlos Orozco Torres');
+  page.get('.sidebar-title').should('have.text', 'Sergio Carlos Orozco Torres')
+  page.get('.hamburger').should('not.be.visible')
 });
