435611b4fdd4f75c119792e40851dfcfa6557e89
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-10-26 13:06:58 -0500

    Tag active (#29)
    * Resaltado del tag activo
    
    * Test tags blog y charla

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -2,6 +2,10 @@ it('Blog post se visualiza', () => {
   const page = cy.visit('/blog');
   page.get('head title').should('have.text', 'Blog | SeCOrTo')
   page.get('header h1').should('have.text', 'Blog');
+  page.get('[href="/blog/tags/python"]').should('not.have.class', 'active');
+  page.get('[href="/blog/tags/python"]').click()
+  page.get('[href="/blog/tags/python"]').should('have.class', 'active');
+  page.get('header h1').should('have.text', 'python');
   page.get('[href="/blog/2022-08-14-poo-python"]').click()
   page.get('header h1').should('have.text', 'Programación orientada a objetos en python');
   page.viewport('iphone-6')
--- cypress/e2e/charla.cy.ts
+++ cypress/e2e/charla.cy.ts
@@ -2,6 +2,10 @@ it('Charlas se visualizan', () => {
   const page = cy.visit('/charla');
   page.get('head title').should('have.text', 'Charlas | SeCOrTo')
   page.get('header h1').should('have.text', 'Charlas');
+  page.get('[href="/charla/tags/containers"]').should('not.have.class', 'active');
+  page.get('[href="/charla/tags/containers"]').click()
+  page.get('[href="/charla/tags/containers"]').should('have.class', 'active');
+  page.get('header h1').should('have.text', 'containers');
   page.get('[href="/charla/2023-09-27-devcontainers"]').click()
   page.get('header h1').should('have.text', 'Devcontainers en localhost');
 });
