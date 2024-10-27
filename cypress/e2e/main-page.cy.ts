03d2bc52153d77d0f12e4503688e47087bc215c7
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-10-27 14:56:17 -0500

    Revisión testcases (#30)
    * Actualizado cypress
    * Combinados sidebar y main-page
    * Accessibility valida las paginas de listados

--- cypress/e2e/main-page.cy.ts
+++ cypress/e2e/main-page.cy.ts
@@ -1,7 +1,12 @@
 it('Pagina de inicio', () => {
-  const page = cy.visit('/');
+  const page = cy.visit('/')
   page.get('title').should('have.text', 'Página de inicio | SeCOrTo')
-  page.get('h1').should('have.text', 'Soy Sergio Carlos Orozco Torres');
-  page.get('.sidebar-title').should('have.text', 'Sergio Carlos Orozco Torres')
+  page.get('h1').should('have.text', 'Soy Sergio Carlos Orozco Torres')
   page.get('.hamburger').should('not.be.visible')
+  page.viewport('iphone-6')
+  page.get('.sidebar-title').should('not.be.visible')
+  page.get('.hamburger').click()
+  page.get('.sidebar-title').should('have.text', 'Sergio Carlos Orozco Torres')
+  page.get('.hamburger').click()
+  page.get('.sidebar-title').should('not.be.visible')
 });
