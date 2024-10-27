805b536be740af796fec21f04bd8f78adc5554e6
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-10-27 14:50:26 -0500

    Revisión testcases * Actualizado cypress * Combinados sidebar y main-page * Accessibility valida las paginas de listados

--- cypress/e2e/accessibility.cy.ts
+++ cypress/e2e/accessibility.cy.ts
@@ -2,20 +2,39 @@ import { darkMode, lightMode } from "./stubs"
 
 describe("Accessibility tests Dark mode", () => {
   beforeEach(() => {
-      cy.visit("/", darkMode()).get("main")
-      cy.injectAxe()
+
   })
   it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y()
+    const page = cy.visit("/", darkMode()).get("main")
+    cy.injectAxe()
+    page.get('[href="/charla/"]').click()
+    cy.injectAxe()
+    page.get('[href="/blog/"]').click()
+    cy.injectAxe()
+    page.get('[href="/comunidad/"]').click()
+    cy.injectAxe()
+    page.get('[href="/trabajo/"]').click()
+    cy.injectAxe()
+    page.get('[href="/proyecto/"]').click()
+    cy.injectAxe()
   })
 })
 
 describe("Accessibility tests light mode", () => {
-  beforeEach(() => {
-      cy.visit("/", lightMode()).get("main")
-      cy.injectAxe()
-  })
+
   it("Has no detectable accessibility violations on load", () => {
+    const page = cy.visit("/", lightMode()).get("main")
+    cy.injectAxe()
     cy.checkA11y()
+    page.get('[href="/charla/"]').click()
+    cy.injectAxe()
+    page.get('[href="/blog/"]').click()
+    cy.injectAxe()
+    page.get('[href="/comunidad/"]').click()
+    cy.injectAxe()
+    page.get('[href="/trabajo/"]').click()
+    cy.injectAxe()
+    page.get('[href="/proyecto/"]').click()
+    cy.injectAxe()
   })
 })
