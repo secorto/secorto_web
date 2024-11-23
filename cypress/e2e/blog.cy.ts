602e906552107dc8e24a77e4e02fdd30f775e394
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-11-22 23:13:26 -0500

    Cypress commands sin variable cost page

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -1,14 +1,14 @@
 it('Blog post se visualiza', () => {
-  const page = cy.visit('/blog');
-  page.get('head title').should('have.text', 'Blog | SeCOrTo')
-  page.get('header h1').should('have.text', 'Blog');
-  page.get('[href="/blog/tags/python"]').should('not.have.class', 'active');
-  page.get('[href="/blog/tags/python"]').click()
-  page.get('[href="/blog/tags/python"]').should('have.class', 'active');
-  page.get('header h1').should('have.text', 'python');
-  page.get('[href="/blog/2022-08-14-poo-python"]').click()
-  page.get('header h1').should('have.text', 'Programación orientada a objetos en python');
-  page.viewport('iphone-6')
+  cy.visit('/blog');
+  cy.get('head title').should('have.text', 'Blog | SeCOrTo')
+  cy.get('header h1').should('have.text', 'Blog');
+  cy.get('[href="/blog/tags/python"]').should('not.have.class', 'active');
+  cy.get('[href="/blog/tags/python"]').click()
+  cy.get('[href="/blog/tags/python"]').should('have.class', 'active');
+  cy.get('header h1').should('have.text', 'python');
+  cy.get('[href="/blog/2022-08-14-poo-python"]').click()
+  cy.get('header h1').should('have.text', 'Programación orientada a objetos en python');
+  cy.viewport('iphone-6')
   cy.scrollTo('right');
   cy.window().its('scrollX').should('eq', 0)
 });
