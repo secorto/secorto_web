75b10e5ec951e86c405f069862d22a1bc30082c0
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-11-23 13:58:43 -0500

    Fix rss blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,21 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog');
+
   return rss({
-    title: 'Alumno de Astro | Blog',
-    description: 'Mi viaje de aprendizaje de Astro',
+    title: 'Sergio Orozco Torres | Blog',
+    description: 'Sitio web de Sergio Orozco Torres',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>es-co</language>`,
   });
 }
