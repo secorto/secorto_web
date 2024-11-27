2886fba23d179c5d6a6629d52e178ce10aff5f4e
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-11-27 00:58:16 -0500

    All pages i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.about': 'About',
+    'nav.blog': 'Blog',
+    'nav.work': 'Work',
+    'nav.talks': 'Talks',
+    'nav.community': 'Community',
+    'nav.projects': 'Projects'
+  },
+  es: {
+    'nav.about': 'Sobre mi',
+    'nav.work': 'Trabajo',
+    'nav.talks': 'Charlas',
+    'nav.community': 'Comunidad',
+    'nav.projects': 'Proyectos'
+  },
+} as const;
