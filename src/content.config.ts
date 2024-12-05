dff5acb175d37818e88e066206918b80b844f921
Sergio C. Orozco Torres <scot3004@gmail.com>
2024-12-05 07:36:25 -0500

    Blog usando loaders

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**\/[^_]*.md', base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
