17a8dcdb6c2c2d68d375342e9bd17901464d7f44
Sergio Orozco <scot3004@gmail.com>
2025-02-08 10:38:41 -0500

    Tipos genéricos EntriesPath y TagsPath

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,35 @@
 import { getCollection } from "astro:content";
-import type { CollectionKey } from "astro:content";
+import type { CollectionEntry, CollectionKey } from "astro:content";
 type CollectionWithTags = 'blog'|'talk'
 
-export const getEntriesPaths = async (collectionName: CollectionKey) => {
-  const entries = await getCollection(collectionName);
+export type EntriesPath<C extends CollectionKey> =  {
+  params: {
+    id: string
+  },
+  props: {
+    entry: CollectionEntry<C>
+  }
+}
+
+export type TagsPath<C extends CollectionWithTags> =  {
+  params: {
+    tag: string
+  },
+  props: {
+    posts: CollectionEntry<C>[],
+    tags: string[]
+  }
+}
+
+export const getEntriesPaths = async (collectionName: CollectionKey): Promise<EntriesPath<CollectionKey>[]> =>  {
+  const entries: CollectionEntry<CollectionKey>[] = await getCollection<CollectionKey>(collectionName);
   return entries.map(entry => ({
     params: { id: entry.id }, props: { entry },
   }));
 }
 
-export const getTagsPaths = async (collectionName: CollectionWithTags) => {
-  const allPosts = (await getCollection(collectionName)).sort((a, b)=> b.id.localeCompare(a.id));
+export async function getTagsPaths<C extends CollectionWithTags>(collectionName: C): Promise<TagsPath<CollectionWithTags>[]> {
+  const allPosts = (await getCollection<CollectionWithTags>(collectionName)).sort((a, b)=> b.id.localeCompare(a.id));
   const uniqueTags = [...new Set(allPosts.map((post) => post.data.tags).flat())].sort((a, b)=> a.localeCompare(b));
 
   return uniqueTags.map((tag) => {
