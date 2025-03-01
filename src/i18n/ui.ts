0212d7c4d8bfb736dc4e4072a006c3ca29e6a36c
Sergio Orozco <scot3004@gmail.com>
2025-03-01 13:42:50 -0500

    Squashed commit of the following:
    commit 1f9f026d7350ae41352a53b22a4249c3931d3ba8
    Author: Sergio C. Orozco Torres <scot3004@gmail.com>
    Date:   Tue Feb 18 14:46:22 2025 -0500
    
        getEntriesPaths and getTagsPaths on english pages
    
    commit 6f75759dacf10df39bf08110cf08799821bff24b
    Author: Sergio Orozco <scot3004@gmail.com>
    Date:   Sat Feb 8 11:30:05 2025 -0500
    
        Squashed commit of the following:
    
        commit 2023440e7bce7700db0dee26bb955fb1a6965112
        Author: Sergio Orozco <scot3004@gmail.com>
        Date:   Sat Feb 8 11:04:17 2025 -0500
    
            Utils path in a specific file
    
        commit 905082e4e2991aa19e2312e36369f4b9576c785f
        Author: Sergio Orozco <scot3004@gmail.com>
        Date:   Sat Feb 8 10:56:13 2025 -0500
    
            EntriesStaticPaths y TagsStaticPaths como alias para que se vea mas limpio
    
        commit 17a8dcdb6c2c2d68d375342e9bd17901464d7f44
        Author: Sergio Orozco <scot3004@gmail.com>
        Date:   Sat Feb 8 10:38:41 2025 -0500
    
            Tipos genéricos EntriesPath y TagsPath
    
        commit 612c262b4a40ded1e3ae855ce6b263886c6f2932
        Author: Sergio Orozco <scot3004@gmail.com>
        Date:   Sat Feb 8 00:26:53 2025 -0500
    
            getStaticPaths con funciones getEntriesPaths y getTagsPaths
    
    commit 32e20d5086483b955351fb18d39adfaabe14452c
    Author: Sergio Orozco <scot3004@gmail.com>
    Date:   Fri Feb 7 22:38:18 2025 -0500
    
        About talk was in spanish for english version
    
    commit bd4613804c900333f91442c4cb5c47ef4738e2d9
    Author: Sergio Orozco <scot3004@gmail.com>
    Date:   Fri Feb 7 22:07:21 2025 -0500
    
        Language picker
    
    commit f51eacc9891869dcc5151e23208007db365af0e4
    Author: Sergio Orozco <scot3004@gmail.com>
    Date:   Fri Feb 7 21:28:42 2025 -0500
    
        English astro 5 migration
    
    commit a5c36f6862aee07f0d4e2afe2f8825d6746cd650
    Author: Sergio C. Orozco Torres <scot3004@gmail.com>
    Date:   Wed Nov 27 00:58:16 2024 -0500
    
        All pages i18n
    
    commit 7903b1ca3ab1dba6d305ae2abfbcce8beb16e5ea
    Author: Sergio C. Orozco Torres <scot3004@gmail.com>
    Date:   Tue Nov 26 22:44:42 2024 -0500
    
        Lang en html tag SiteLayout
    
    commit f7817ead8b06c018c3584e287f9ce9c0dd48d61f
    Author: Sergio C. Orozco Torres <scot3004@gmail.com>
    Date:   Tue Oct 1 22:19:28 2024 +0000
    
        Primer acercamiento index en ingles

---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const languages = {
+  en: 'English',
+  es: 'Spanish'
+};
+
+export const ui = {
+  en: {
+    'nav.about': 'About',
+    'nav.blog': 'Blog',
+    'nav.work': 'Work',
+    'nav.talks': 'Talks',
+    'nav.community': 'Community',
+    'nav.projects': 'Projects'
+  },
+  es: {
+    'nav.about': 'Sobre mi',
+    'nav.blog': 'Blog',
+    'nav.work': 'Trabajo',
+    'nav.talks': 'Charlas',
+    'nav.community': 'Comunidad',
+    'nav.projects': 'Proyectos'
+  },
+} as const;
