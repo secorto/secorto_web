00313e42c36eaa2bd6d1699efd909d60fb27ff51
Sergio C. Orozco Torres <scot3004@gmail.com>
2025-03-25 10:08:32 -0500

    Enabled prefix default locale

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -1,12 +1,12 @@
 it('Blog post se visualiza', () => {
-  cy.visit('/blog');
+  cy.visit('/es/blog');
   cy.get('head title').should('have.text', 'Blog | SeCOrTo')
   cy.get('header h1').should('have.text', 'Blog');
-  cy.get('[href="/blog/tags/python"]').should('not.have.class', 'active');
-  cy.get('[href="/blog/tags/python"]').click()
-  cy.get('[href="/blog/tags/python"]').should('have.class', 'active');
+  cy.get('[href="/es/blog/tags/python"]').should('not.have.class', 'active');
+  cy.get('[href="/es/blog/tags/python"]').click()
+  cy.get('[href="/es/blog/tags/python"]').should('have.class', 'active');
   cy.get('header h1').should('have.text', 'python');
-  cy.get('[href="/blog/2022-08-14-poo-python"]').click()
+  cy.get('[href="/es/blog/2022-08-14-poo-python"]').click()
   cy.get('header h1').should('have.text', 'Programación orientada a objetos en python');
   cy.viewport('iphone-6')
   cy.scrollTo('right');
