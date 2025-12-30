ccde0d231b640e8338d9f965191f503c8cd07158
Sergio Orozco <scot3004@gmail.com>
2025-12-29 19:27:02 -0500

    Corrección translations para proyectos donde se marcaba como no traducido cuando si lo estaba

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -3,7 +3,7 @@ import { buildTranslationMap } from "./buildTranslationMap";
 export const translations = {
   blog: await buildTranslationMap("blog"),
   work: await buildTranslationMap("work"),
-  projects: await buildTranslationMap("projects"),
+  project: await buildTranslationMap("projects"),
   community: await buildTranslationMap("community"),
   talk: await buildTranslationMap("talk"),
 };
