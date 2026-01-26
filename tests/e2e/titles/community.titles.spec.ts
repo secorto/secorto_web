74a910689c2fe646039253905da410e1c6793920
Sergio Orozco <scot3004@gmail.com>
2026-01-25 20:00:58 -0500

    Tests project community work

---
+++ tests/e2e/titles/community.titles.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test'
+import { ContentListPage } from '../../pages/ContentListPage'
+import { ui, type UILanguages } from '@i18n/ui'
+import { sectionsConfig, type SectionType } from '@config/sections'
+
+const locales = ['es', 'en'] as const
+const section: SectionType = 'community'
+
+test.describe('Community list title', () => {
+  locales.forEach((locale) => {
+    test(`community list title is correct (${locale})`, async ({ page }) => {
+      const list = new ContentListPage(page)
+      await list.goto(locale as UILanguages, section)
+
+      const title = list.headerTitle()
+      await expect(title).toBeVisible()
+      const key = sectionsConfig[section].translationKey as keyof typeof ui['es']
+      const expected = ui[locale][key]
+      await expect(title).toHaveText(expected)
+    })
+  })
+})
