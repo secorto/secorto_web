b3986ce63cdbcfa59fb45503fcabab005bc044e0
Sergio Orozco <scot3004@gmail.com>
2026-01-30 23:52:19 -0500

    a11y action

--- tests/actions/A11yActions.ts
+++ tests/actions/A11yActions.ts
@@ -3,8 +3,10 @@ import type { Page } from '@playwright/test'
 
 export const DEFAULT_EXCLUDES = ['[data-netlify-deploy-id]']
 
-export async function checkA11y(page: Page, excludes: string[] = DEFAULT_EXCLUDES) {
-  const builder = new AxeBuilder({ page })
-  for (const ex of excludes) builder.exclude(ex)
-  return await builder.analyze()
-}
+export const checkA11y =
+  (excludes: string[] = DEFAULT_EXCLUDES) =>
+  async ({ page }: { page: Page }) => {
+    const builder = new AxeBuilder({ page })
+    for (const ex of excludes) builder.exclude(ex)
+    return await builder.analyze()
+  }
--- tests/e2e/a11y/charla.a11y.spec.ts
+++ tests/e2e/a11y/charla.a11y.spec.ts
@@ -8,7 +8,7 @@ test.describe('A11y - Charlas', () => {
   locales.forEach((locale) => {
     test(`charla list a11y (${locale})`, async ({ page }) => {
       await page.goto(getURLForSection('talk', locale))
-      const listingResults = await checkA11y(page)
+      const listingResults = await checkA11y()({page})
       expect(listingResults.violations).toEqual([])
     })
 
@@ -16,7 +16,7 @@ test.describe('A11y - Charlas', () => {
       const talksTagRoute = `${getURLForSection('talk', locale)}/tags/containers`
       await page.goto(talksTagRoute)
 
-      const tagResults = await checkA11y(page)
+      const tagResults = await checkA11y()({page})
       expect(tagResults.violations).toEqual([])
     })
 
@@ -25,11 +25,11 @@ test.describe('A11y - Charlas', () => {
       const talksRoute = getURLForSection('talk', locale)
       const talksPostRoute = `${talksRoute}/${postSlug}`
       await page.goto(talksPostRoute)
-      const detailResults = await checkA11y(page, [
+      const detailResults = await checkA11y([
         '[data-testid="post-video"]',
         '[data-testid="post-slide"]',
         '[data-testid="comments-section"]'
-      ])
+      ])({page})
       expect(detailResults.violations).toEqual([])
     })
   })
