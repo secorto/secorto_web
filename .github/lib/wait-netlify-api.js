c4a155a1aa5a2705accf331445f840b09e152b20
Sergio Orozco <scot3004@gmail.com>
2026-02-06 00:09:40 -0500

    Vitest coverage

--- .github/lib/wait-netlify-api.js
+++ .github/lib/wait-netlify-api.js
@@ -1,7 +1,6 @@
 // Netlify API thin wrapper
 
 async function listDeploys(siteId, token) {
-  if (typeof fetch !== 'function') throw new Error('Global fetch is not available')
   const res = await fetch(`https://api.netlify.com/api/v1/sites/${siteId}/deploys?per_page=20`, {
     headers: { Authorization: 'Bearer ' + token }
   })
