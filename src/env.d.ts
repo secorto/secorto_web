8d9cb72a43844b5faf80cad3ea665e61e91cdd58
Sergio Orozco <scot3004@gmail.com>
2026-02-07 22:15:21 -0500

    Disable eslint on env.d.ts Disabled rule import/no-unresolved and explained the reason

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
+// This file imports Dynamic types from astro and they are unavailable until the build step, so we need to ignore imports not resolved
+// eslint-disable import/no-unresolved
 import "../.astro/types.d.ts"
 /// <reference types="astro/client" />
