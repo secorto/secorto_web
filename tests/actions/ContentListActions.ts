9b8f2fdf80bc7a470a205bde08c96c20bab249f0
Sergio Orozco <scot3004@gmail.com>
2026-05-17 13:43:49 -0500

    Actions wrap step

--- tests/actions/ContentListActions.ts
+++ tests/actions/ContentListActions.ts
@@ -2,14 +2,16 @@ import { getSectionRoute } from '@utils/sections'
 import type { SectionType } from '@domain/section'
 import type { UILanguages } from '@i18n/ui'
 import type { ContentListPage } from '@tests/pages/ContentListPage'
-import type { Page } from '@playwright/test'
+import { test, type Page } from '@playwright/test'
 
-export async function goto(page: Page, locale: UILanguages, collection: SectionType) {
-  const route = getSectionRoute(collection, locale)
-  await page.goto(`/${locale}/${route}`)
+export async function openCollectionList(page: Page, locale: UILanguages, collection: SectionType) {
+  await test.step(`Open ${collection} list in ${locale}`, async () => {
+    const route = getSectionRoute(collection, locale)
+    await page.goto(`/${locale}/${route}`)
+  })
 }
 
-export function getItemPath(locale: UILanguages, collection: SectionType, itemPath: string) {
+function getItemPath(locale: UILanguages, collection: SectionType, itemPath: string) {
   const route = getSectionRoute(collection, locale)
   const href = `/${locale}/${route}/${itemPath}`
   const pattern = `${locale}/${route}/`
@@ -17,9 +19,11 @@ export function getItemPath(locale: UILanguages, collection: SectionType, itemPa
 }
 
 export async function openItem(list: ContentListPage, locale: UILanguages, collection: SectionType, itemPath: string) {
-  const { pattern, href } = getItemPath(locale, collection, itemPath)
-  await Promise.all([
-    list.page.waitForURL(new RegExp(pattern)),
-    list.page.locator(`[href="${href}"]`).click()
-  ])
+  await test.step(`Open ${collection} item ${itemPath} in ${locale}`, async () => {
+    const { pattern, href } = getItemPath(locale, collection, itemPath)
+    await Promise.all([
+      list.page.waitForURL(new RegExp(pattern)),
+      list.page.locator(`[href="${href}"]`).click()
+    ])
+  })
 }
