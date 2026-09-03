61778d099f0ab93fd09f8c3f08f1cf1b59d69f51
Sergio Orozco <scot3004@gmail.com>
2026-09-03 10:52:19 -0500

    Implemented shared filter for collection

---
+++ packages/i18n/src/astro/filters.ts
@@ -0,0 +1,11 @@
+import type { GenericCollectionEntry } from "@secorto/i18n"
+
+export type Draftable = {
+  draft?: boolean
+}
+
+export const availableAtLocale = <TLocale extends string>(locale: TLocale) =>
+  <TSection extends string,
+    TEntry extends GenericCollectionEntry<TSection, Draftable>
+  >(entry: TEntry) => entry.id.startsWith(`${locale}/`) &&
+      entry.data.draft !== true
